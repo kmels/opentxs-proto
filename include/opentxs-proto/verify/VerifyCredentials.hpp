a6c55d3f0a19708a68175eb8f9af5de92e02253a
Carlos Lopez <c.lopez@kmels.net>
2018-11-22 01:09:29 +0000

    Allow v5 ContactData for Credential v4

--- include/opentxs-proto/verify/VerifyCredentials.hpp
+++ include/opentxs-proto/verify/VerifyCredentials.hpp
@@ -23,7 +23,7 @@ static const VersionMap CredentialAllowedContactData = {
     {1, {1, 1}},
     {2, {2, 2}},
     {3, {3, 3}},
-    {4, {4, 4}},
+    {4, {4, 5}},
     {5, {5, 5}},
 };
 static const VersionMap CredentialAllowedVerification = {
